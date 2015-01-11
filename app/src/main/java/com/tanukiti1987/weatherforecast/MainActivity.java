59d7159677549c415091db0269906d90bb9d35ec
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2015-01-12 01:43:31 +0900

    Initial commit

---
+++ app/src/main/java/com/tanukiti1987/weatherforecast/MainActivity.java
@@ -0,0 +1,73 @@
+package com.tanukiti1987.weatherforecast;
+
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView textView;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler = new Handler();
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tv_main);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    final String data = WeatherApi.getWeather(MainActivity.this, "400040");
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(data);
+                        }
+                    });
+                } catch (final IOException e) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        };
+        thread.start();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/tanukiti1987/weatherforecast/WeatherApi.java
@@ -0,0 +1,41 @@
+package com.tanukiti1987.weatherforecast;
+
+import android.content.Context;
+import android.net.http.AndroidHttpClient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by tanukiti1987 on 2015/01/12.
+ */
+public class WeatherApi {
+    private static final String USER_AGENT = "WheatherForecasts Sample";
+    private static final String URL =
+            "http://weather.livedoor.com/forecast/webservice/json/v1?city=";
+
+    public static String getWeather(Context context, String pointId) throws IOException {
+        AndroidHttpClient client = AndroidHttpClient.newInstance(USER_AGENT, context);
+        HttpGet get = new HttpGet(URL + pointId);
+
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            HttpResponse response = client.execute(get);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } finally {
+            client.close();
+        }
+
+        return sb.toString();
+    }
+}
