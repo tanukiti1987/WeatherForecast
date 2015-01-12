d227d3045dfa13f7501da99b5c69945fe1307b34
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2015-01-12 17:29:16 +0900

    Add ImageLoaderTask class

---
+++ app/src/main/java/com/tanukiti1987/weatherforecast/ImageLoaderTask.java
@@ -0,0 +1,63 @@
+package com.tanukiti1987.weatherforecast;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+
+import java.io.IOException;
+
+/**
+ * Created by tanukiti1987 on 2015/01/12.
+ */
+public class ImageLoaderTask extends AsyncTask<ImageView, Void, Bitmap> {
+    private static final String USER_AGENT = "WeatherForecasts Sample";
+
+    private final Context context;
+
+    Exception exception;
+    ImageView imageView;
+
+    public ImageLoaderTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected Bitmap doInBackground(ImageView... params) {
+        imageView = params[0];
+        String url = (String) imageView.getTag();
+        try {
+            return getImage(context, url);
+        } catch (IOException e) {
+            exception = e;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private static Bitmap getImage(Context context, String uri) throws IOException {
+        AndroidHttpClient client = AndroidHttpClient.newInstance(USER_AGENT, context);
+
+        HttpGet get = new HttpGet(uri);
+
+        try {
+            HttpResponse response = client.execute(get);
+            return BitmapFactory.decodeStream(response.getEntity().getContent());
+        } finally {
+            client.close();
+        }
+    }
+}
